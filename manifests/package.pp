60f3828a76163424e571a7ca349e1c6cfbf22da1
Matthew Schmitt <hundredacres.rm@yahoo.com>
2017-10-24 18:05:05 +0000

    Adding RedHat support to package install Updating template to remove doc_type param and added tags param

--- manifests/package.pp
+++ manifests/package.pp
@@ -21,6 +21,24 @@ class filebeats::package {
         ensure => present,
       }
     }
+    'RedHat': {
+      exec { 'Import Elastic GPG key':
+        command => '/bin/rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch',
+        cwd     => '/etc/pki/rpm-gpg',
+	unless  => '/bin/rpm -qi GPG-KEY-elasticsearch',
+        before  => File['elastic-5.x repo'],
+      }
+      file { 'elastic-5.x repo':
+        ensure => 'present',
+        path   => '/etc/yum.repos.d/elastic.repo',
+	owner  => 'root',
+	group  => 'root',
+	source => 'puppet:///modules/filebeats/elastic.repo',
+      }
+      package {'filebeat':
+        ensure => present,
+      }
+    }
     default: {
       fail('Could not configure apt resource for elasticsearch filebeats')
     }
