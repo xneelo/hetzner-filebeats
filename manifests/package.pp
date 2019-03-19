fd795c86533560695190e2647b0dc83b86a88b4f
Matthew Schmitt <matthew.schmitt@riskiq.net>
2019-03-19 12:44:46 -0700

    Setting Redhat behavior to Debian

--- manifests/package.pp
+++ manifests/package.pp
@@ -9,21 +9,9 @@ class filebeats::package {
       }
     }
     'RedHat': {
-      exec { 'Import Elastic GPG key':
-        command => '/bin/rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch',
-        cwd     => '/etc/pki/rpm-gpg',
-        unless  => '/bin/rpm -qa gpg-pubkey\* --qf "%{name}-%{version}-%{release}-%{summary}\n" | grep -i elasticsearch',
-        before  => File['elastic-5.x repo'],
-      }
-      file { 'elastic-5.x repo':
-        ensure => 'present',
-        path   => '/etc/yum.repos.d/elastic.repo',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/filebeats/elastic.repo',
-      }
       package {'filebeat':
         ensure => present,
+        require => Class['::elastic_stack::repo']
       }
     }
     default: {
